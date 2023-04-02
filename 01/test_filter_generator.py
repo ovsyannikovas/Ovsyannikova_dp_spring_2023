8a8c46c85f46810f9778cf5ddeeec25af48a3207
ovsyannikovas <s.ovsyannikova7@gmail.com>
2023-04-02 16:34:18 +0300

    Изменены проверки в test_filter_generator.py

--- 01/test_filter_generator.py
+++ 01/test_filter_generator.py
@@ -6,36 +6,31 @@ from filter_generator import filter_generator
 class TestGenerator(unittest.TestCase):
     def test_found(self):
         file = io.StringIO('а Роза упала на лапу Азора')
-        gen = filter_generator(file, ['роза'])
-        self.assertEqual(next(gen), 'а Роза упала на лапу Азора')
-        with self.assertRaises(Exception) as context:
-            next(gen)
-        self.assertTrue(isinstance(context.exception, StopIteration))
+        gen = filter_generator(['роза'], fileobject=file)
+        self.assertEqual(list(gen), ['а Роза упала на лапу Азора'])
+
+    def test_found_several(self):
+        file = io.StringIO('а Роза упала на лапу Азора\n алая РОЗА\n Азор')
+        gen = filter_generator(['РоЗа'], fileobject=file)
+        self.assertEqual(list(gen), ['а Роза упала на лапу Азора\n',
+                                     ' алая РОЗА\n'])
 
     def test_not_found(self):
         file = io.StringIO('а Роза упала на лапу Азора')
-        gen = filter_generator(file, ['роз'])
-        with self.assertRaises(Exception) as context:
-            next(gen)
-        self.assertTrue(isinstance(context.exception, StopIteration))
+        gen = filter_generator(['роз'], fileobject=file)
+        self.assertEqual(list(gen), [])
 
     def test_empty_file(self):
         file = io.StringIO('')
-        gen = filter_generator(file, ['роза'])
-        with self.assertRaises(Exception) as context:
-            next(gen)
-        self.assertTrue(isinstance(context.exception, StopIteration))
+        gen = filter_generator(['роза'], fileobject=file)
+        self.assertEqual(list(gen), [])
 
     def test_empty_words_list(self):
         file = io.StringIO('а Роза упала на лапу Азора')
-        gen = filter_generator(file, [])
-        with self.assertRaises(Exception) as context:
-            next(gen)
-        self.assertTrue(isinstance(context.exception, StopIteration))
+        gen = filter_generator([], fileobject=file)
+        self.assertEqual(list(gen), [])
 
     def test_words_list_with_empty_string(self):
         file = io.StringIO('а Роза упала на лапу Азора')
-        gen = filter_generator(file, [''])
-        with self.assertRaises(Exception) as context:
-            next(gen)
-        self.assertTrue(isinstance(context.exception, StopIteration))
+        gen = filter_generator([''], fileobject=file)
+        self.assertEqual(list(gen), [])
