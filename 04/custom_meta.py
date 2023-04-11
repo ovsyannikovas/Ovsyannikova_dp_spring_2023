8df1c491b1d1bdfe5895409ade06aa01c56d18be
ovsyannikovas <s.ovsyannikova7@gmail.com>
2023-04-11 21:22:07 +0300

    Убраны assert в custom_meta.py

--- 04/custom_meta.py
+++ 04/custom_meta.py
@@ -27,22 +27,3 @@ class CustomClass(metaclass=CustomMeta):
 
     def __str__(self):
         return 'Custom_by_metaclass'
-
-
-assert CustomClass.custom_x == 50
-# CustomClass.x  # ошибка
-
-inst = CustomClass()
-assert inst.custom_x == 50
-assert inst.custom_val == 99
-assert inst.custom_line() == 100
-assert str(inst) == 'Custom_by_metaclass'
-
-# inst.x  # ошибка
-# inst.val  # ошибка
-# inst.line()  # ошибка
-# inst.yyy  # ошибка
-
-inst.dynamic = 'added later'
-assert inst.custom_dynamic == 'added later'
-# inst.dynamic  # ошибка
