7b39b70a86379c6ec7c62943ff9de1c2f9d71c6a
ovsyannikovas <s.ovsyannikova7@gmail.com>
2023-04-11 20:03:30 +0300

    Написаны дескрипторы

---
+++ 04/descriptors.py
@@ -0,0 +1,56 @@
+class Integer:
+    def __init__(self):
+        self.__value = None
+
+    def __set__(self, instance, value):
+        if instance(value, int):
+            self.__value = value
+        else:
+            raise Exception('Недопустимое значение для целого числа.')
+
+    def __get__(self, instance, owner):
+        return self.__value
+
+
+class String:
+    def __init__(self):
+        self.__value = None
+
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            self.__value = value
+        else:
+            raise Exception('Недопустимое значение для строки.')
+
+    def __get__(self, instance, owner):
+        return self.__value
+
+
+class PositiveInteger:
+    def __init__(self):
+        self.__value = None
+
+    def __set__(self, instance, value):
+        if isinstance(value, int) and value > 0:
+            self.__value = value
+        else:
+            raise Exception('Недопустимое значение для положительного числа.')
+
+    def __get__(self, instance, owner):
+        return self.__value
+
+
+class Data:
+    num = Integer()
+    name = String()
+    price = PositiveInteger()
+
+    def __init__(self, num, name, price):
+        self.num = num
+        self.name = name
+        self.price = price
+
+
+if __name__ == "__main__":
+    data = Data(5, 'Sveta', 9999)
+    print(data.num, data.name, data.price)
