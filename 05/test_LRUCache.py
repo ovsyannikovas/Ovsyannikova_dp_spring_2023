7f70c7860f3431be8e2b04edcd5e6839bd6f62f3
ovsyannikovas <s.ovsyannikova7@gmail.com>
2023-04-18 20:50:18 +0300

    Написаны первые тесты

--- 05/test_LRUCache.py
+++ 05/test_LRUCache.py
@@ -6,7 +6,7 @@ class TestLRUCache(unittest.TestCase):
     def setUp(self):
         self.cache = LRUCache(2)
 
-    def test_1(self):
+    def test_cache_get(self):
         self.cache.set("k1", "val1")
         self.cache.set("k2", "val2")
 
@@ -14,10 +14,14 @@ class TestLRUCache(unittest.TestCase):
         self.assertTrue(self.cache.get("k2") == "val2")
         self.assertTrue(self.cache.get("k1") == "val1")
 
-    def test_2(self):
-        self.test_1()
+    def test_lru_mechanism(self):
+        self.test_cache_get()
         self.cache.set("k3", "val3")
 
         self.assertTrue(self.cache.get("k3") == "val3")
         self.assertTrue(self.cache.get("k2") is None)
-        self.assertTrue(self.cache.get("k1") == "val1")
+
+    def test_new_value(self):
+        self.test_cache_get()
+        self.cache.set("k3", "test")
+        self.assertTrue(self.cache.get("k3") == "test")
