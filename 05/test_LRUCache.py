624228572ac2483a64a37c85e848cb43f0eec898
ovsyannikovas <s.ovsyannikova7@gmail.com>
2023-05-07 11:34:54 +0300

    Добавлен тест

--- 05/test_LRUCache.py
+++ 05/test_LRUCache.py
@@ -1,5 +1,4 @@
 import unittest
-from collections import deque
 
 from LRUCache import LRUCache
 
@@ -9,10 +8,9 @@ class TestLRUCache(unittest.TestCase):
         self.cache = LRUCache(2)
 
     def test_attrs(self):
-        self.assertTrue(isinstance(self.cache.cache, dict))
-        self.assertTrue(self.cache.cache == {})
-        self.assertTrue(isinstance(self.cache.keys_deque, deque))
-        self.assertTrue(len(self.cache.keys_deque) == 0)
+        self.assertTrue(isinstance(self.cache.dic, dict))
+        self.assertTrue(self.cache.dic == {})
+        ...
         self.assertTrue(isinstance(self.cache.limit, int))
         self.assertTrue(self.cache.limit == 2)
 
@@ -24,12 +22,11 @@ class TestLRUCache(unittest.TestCase):
         self.assertTrue(self.cache.get("k2") == "val2")
         self.assertTrue(self.cache.get("k1") == "val1")
 
-    def test_lru_mechanism(self):
-        self.test_cache_get()
         self.cache.set("k3", "val3")
 
         self.assertTrue(self.cache.get("k3") == "val3")
         self.assertTrue(self.cache.get("k2") is None)
+        self.assertTrue(self.cache.get("k1") == "val1")
 
     def test_new_value(self):
         self.test_cache_get()
@@ -40,6 +37,7 @@ class TestLRUCache(unittest.TestCase):
         cache = LRUCache(1)
         cache.set("k1", "val1")
         cache.set("k2", "val2")
+
         self.assertTrue(cache.get("k2") == "val2")
         self.assertTrue(cache.get("k1") is None)
 
@@ -48,7 +46,18 @@ class TestLRUCache(unittest.TestCase):
         self.cache.set("k2", "val2")
         self.cache.set("k3", "val3")
         self.cache.set("k4", "val4")
+
         self.assertTrue(self.cache.get("k1") is None)
         self.assertTrue(self.cache.get("k2") is None)
         self.assertTrue(self.cache.get("k3") == "val3")
         self.assertTrue(self.cache.get("k4") == "val4")
+
+    def test_change_val_of_existent_and_displacement(self):
+        self.cache.set("k1", "val1")
+        self.cache.set("k2", "val2")
+        self.cache.set("k1", "new_val1")
+        self.cache.set("k3", "val3")
+
+        self.assertTrue(self.cache.get("k1") == "new_val1")
+        self.assertTrue(self.cache.get("k2") is None)
+        self.assertTrue(self.cache.get("k3") == "val3")
