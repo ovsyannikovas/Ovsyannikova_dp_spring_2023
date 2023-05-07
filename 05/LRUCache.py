2047acd49a0dc1c5ae5678218180bffa6a05d639
ovsyannikovas <s.ovsyannikova7@gmail.com>
2023-05-07 11:16:09 +0300

    Новая реализация LRUCache

--- 05/LRUCache.py
+++ 05/LRUCache.py
@@ -1,25 +1,51 @@
-from collections import deque
+class Node:
+
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
 
 
 class LRUCache:
 
     def __init__(self, limit=42):
-        self.cache = {}
-        self.keys_deque = deque()
         self.limit = limit
+        self.dic = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key):
-        if key not in self.keys_deque:
-            return None
-        else:
-            val = self.keys_deque[self.keys_deque.index(key)]
-            self.keys_deque.remove(val)
-            self.keys_deque.append(val)
-            return self.cache[key]
+        if key in self.dic:
+            n = self.dic[key]
+            self._remove_node(n)
+            self._add_node(n)
+            return n.val
+        return None
 
     def set(self, key, value):
-        self.cache[key] = value
-        self.keys_deque.append(key)
-        if len(self.keys_deque) > self.limit:
-            self.cache.pop(self.keys_deque[0])
-            self.keys_deque.popleft()
+        if key in self.dic:
+            self._remove_node(self.dic[key])
+        new_node = Node(key, value)
+        self._add_node(new_node)
+        self.dic[key] = new_node
+
+        if len(self.dic) > self.limit:
+            new_node = self.head.next
+            self._remove_node(new_node)
+            del self.dic[new_node.key]
+
+    def _remove_node(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+    def _add_node(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
